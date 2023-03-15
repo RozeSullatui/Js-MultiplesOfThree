1514f9ac75c22dd5485da99a29101041eaff4f6a
RozeSullatui <roze23vpa40769@gmail.com>
2023-03-15 17:17:50 +0900

    四角から丸への変化できた

--- Counter/main.js
+++ Counter/main.js
@@ -1,10 +1,21 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
-
+const minusButton = document.getElementById("minus-button")
+const TwoTimesButton = document.getElementById("TwoTimes-button")
 
 let count = 0
 
 plusButton.onclick = function() {
     count++
+    display.textContent = count
+}
+
+minusButton.onclick = function() {
+    count--
+    display.textContent = count
+}
+
+TwoTimesButton.onclick = function() {
+    count=count*2
     display.textContent = count
 }
